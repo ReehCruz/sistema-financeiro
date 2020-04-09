4be4e05a11653bf84c42ea7b3a09154e062d1c2d
Rebeca Cruz <rebecacruz@me.com>
2020-04-09 18:03:53 -0300

    Exclusao de lancamentos do banco de dados

--- src/com/rebeca/financeiro/view/ConsultaLancamentoBean.java
+++ src/com/rebeca/financeiro/view/ConsultaLancamentoBean.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
 
 import com.rebeca.financeiro.model.Lancamento;
+import com.rebeca.financeiro.util.FacesUtil;
 import com.rebeca.financeiro.util.HibernateUtil;
 
 @ManagedBean
 public class ConsultaLancamentoBean implements Serializable {
 	
-private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	private Lancamento lancamentoSelecionado;
 	
 	@SuppressWarnings("unchecked")
 	@PostConstruct
@@ -29,9 +33,35 @@ private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
 		
 		session.close();
 	}
+	
+	public void excluir() {
+		if (this.lancamentoSelecionado.isPago()) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Lançamento já foi pago e não pode ser excluído.");
+		} else {
+			Session session = HibernateUtil.getSession();
+			Transaction trx = session.beginTransaction();
+			
+			session.delete(this.lancamentoSelecionado);
+			
+			trx.commit();
+			session.close();
+			
+			this.inicializar();
+			
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, "Lançamento excluído com sucesso!");
+		}
+	}
 
 	public List<Lancamento> getLancamentos() {
 		return lancamentos;
+	}
+	
+	public Lancamento getLancamentoSelecionado() {
+		return lancamentoSelecionado;
+	}
+
+	public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
+		this.lancamentoSelecionado = lancamentoSelecionado;
 	}
 	
 }
